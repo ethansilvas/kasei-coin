b980d4dfcd98d129ab61284a1faff075542b64b6
Ethan Silvas <ethansilvas@gmail.com>
2023-03-10 10:36:37 -0800

    kaseicoin and kaseicoincrowdsale

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -8,11 +8,13 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 // Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
 // * Crowdsale
 // * MintedCrowdsale
-contract KaseiCoinCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale {
     
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
     constructor(
-        // YOUR CODE HERE!
+        uint256 rate,
+        address payable wallet, 
+        KaseiCoin token
     ) public Crowdsale(rate, wallet, token) {
         // constructor can stay empty
     }
